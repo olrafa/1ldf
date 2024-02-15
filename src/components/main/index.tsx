bde849f7147080c47e1096c2cdd49421e31e4788
Rafael Olivares <olrafael@mailfence.com>
2024-02-15 15:22:46 -0300

    test

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -5,6 +5,7 @@ import useGetGuests from "../../hooks/useGetGuests";
 import Loader from "../loader";
 import useGetArticles from "../../hooks/useGetArticles";
 import ArticleCard from "../list/ArticleCard";
+import { Helmet } from "react-helmet-async";
 
 const Main = (): ReactElement => {
   const { data: guests = [], isLoading } = useGetGuests();
@@ -41,6 +42,22 @@ const Main = (): ReactElement => {
       {latestFilm && (
         <ArticleCard category="film" article={latestFilm} cover={true} />
       )}
+      <Helmet>
+        <meta property="og:title" content="1 Livro, 1 Disco, 1 Filme" />
+        <meta
+          property="og:description"
+          content="Um papo sobre o livro, disco e filme que marcaram a vida dos nossos convidados."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://1ldf.vercel.app/assets/profile-F1fwxUuW.jpg"
+        />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+      </Helmet>
     </div>
   );
 };
